c2f5762f001d22113f24b54a610516262d997e30
Nick Boer <nboer@c3a3c10.42berlin.de>
2025-09-04 17:26:36 +0200

    updat

---
+++ inc/Client.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Client.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/04 14:06:42 by nboer             #+#    #+#             */
+/*   Updated: 2025/09/04 16:08:45 by nboer            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// 2 create a socket 
+// 3 connect to the server
+// 4 Send and receive data
+// 5. disconnect 
+
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <iostream>
+
+class Client {
+	private:
+
+		
+	public:
+		Client();
+		~Client();
+};
+
+#endif
