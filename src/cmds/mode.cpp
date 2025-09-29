4a9fe46d5eebc8001d30bc492014d2716544fbde
Alexandra Roux <aroux@c4a9c4.42berlin.de>
2025-09-29 10:29:50 +0200

    update mode pseudocode

--- src/cmds/mode.cpp
+++ src/cmds/mode.cpp
@@ -8,11 +8,14 @@ MODE:
 	- MODE <channel> [+|-] o <user>
 	- MODE <channel> +k <psswd>
 	- MODE <channel> +l <limit>
+	- MODE <channel> <unknown char>: ERR_UNKNOWNMODE (472): "472 <nick> <char> :is unknown mode char to me"
 
 	check:
 	- at least <channel> and option param. if not: ERR_NEEDMOREPARAMS
 	- user is on the channel. if not: ERR_NOTONCHANNEL
 	- user has chanop privileges. if not: ERR_CHANOPRIVSNEEDED
+	- ERR_UMODEUNKNOWNFLAG (501) - Invalid user mode flag (for +i, +o, +w) : "501 :Unknown MODE flag"
+	- ERR_USERSDONTMATCH (502) - Trying to change another user's modes (for +i, +o, +w) : "502 :Cannot change mode for other users"
 
 	+i:
 	- if !isInviteOnly() -> set to true
@@ -34,6 +37,7 @@ MODE:
 	- must be used /MODE <channel> +k <passwd>. if not: ERR_NEEDMOREPARAMS
 	- if !isReqPassword() -> set to true and _localpass = <passwd>
 	- if isReqPassword() -> leave true and set _localpass = <passwd>
+	- ERR_KEYSET (467) - Channel already has a key set
 
 	-k
 	- if !isReqPasswd() -> ignore
@@ -41,7 +45,7 @@ MODE:
 
 	+o
 	- must be used /MODE <channel> +o <target>. if not: ERR_NEEDMOREPARAMS
-	- check if target is part of channel. if not: ?
+	- check if target is part of channel. if not: ERR_NOTONCHANNEL
 	- add target to list of chanops
 
 	-o
@@ -53,6 +57,7 @@ MODE:
 	- must be used /MODE <channel> +l <limit>. if not: ERR_NEEDMOREPARAMS
 	- if !isUserLimitSet() -> set to true and _userLimit = <limit>
 	- if already true -> ignore
+	- ERR_UNKNOWNMODE (472) - Invalid limit value (non-numeric)
 
 	-l
 	- if already false -> ignore
